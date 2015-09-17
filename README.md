64662ddb4d9fdafa101c718b343dbcf415945ffd
Steffen Graf <S.Graf@gsg-elektronik.de>
2015-09-17 18:31:18 +0200

    Added a bit more readme and fixe build.sh

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# orangepipc
-Kernel and u-boot for orange pi pc with allwinner h3
+# Orange Pi PC
+###Kernel and u-boot for Orange Pi Pc with allwinner h3 
+
+#### Building
+See build.sh before executing it and modify paths and what you want to build.
--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ cp ../../../../../brandy/u-boot-2011.09/u-boot.bin u-boot.fex
 ../../../../../sunxi-tools/fex2bin ../configs/dolphin-p1/sys_config.fex ../configs/dolphin-p1/out/fex/sys_config.bin 
 ../../../pctools/linux/mod_update/update_uboot u-boot.fex ../configs/dolphin-p1/out/fex/sys_config.bin
 
-dd if=u-boot.fex of=/dev/sde bs=1k seek=16400
-cp ../configs/dolphin-p1/out/fex/sys_config.bin $1
+#dd if=u-boot.fex of=/dev/sde bs=1k seek=16400
+#cp ../configs/dolphin-p1/out/fex/sys_config.bin $1
 
-cd linux-3.4
+cd ../../../../../linux-3.4/
 #make orangepipc_defconfig
 make uImage -j4
-cp arch/arm/boot/uImage $1
+#cp arch/arm/boot/uImage $1
 
