707307857a6bf2a96bb76b61bf16ad6f2a45b82b
Steffen Graf <S.Graf@gsg-elektronik.de>
2015-09-24 12:03:19 +0200

    Extended build script and readme

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-# Orange Pi PC
-###Kernel and u-boot for Orange Pi Pc with allwinner h3 
+#Kernel and U-Boot for Orange Pi PC with Allwinner H3 
 
-#### Building
-See build.sh before executing it and modify paths and what you want to build.
+If someone finds out why HDMI is not working, please let me know.
+
+###0. Prerequisites
+arm-linux-gnueabihf- and arm-none-eabi- toolchain has to be installed.
+
+###1. Building U-Boot
+Building U-Boot from SDK with modification for reading a script.bin converted fex file:
+
+```
+./build.sh uboot-all /dev/sde
+````
+
+Where /dev/sde has to be replaced by your SD card.
+
+###2. Edit fex file
+```
+./build.sh fex-edit
+````
+
+###3. Convert and copy fex file
+```
+./build.sh fex-copy /media/sd
+````
+
+Replace /media/sd by the mount point of your SD card.
+
+###4. Building Kernel
+```
+./build.sh kernel-all /media/sd
+````
+
+Replace /media/sd by the mount point of your SD card.
+
--- build.sh
+++ build.sh
@@ -1,18 +1,137 @@
-#!/bin/sh
+#!/bin/bash
+# This scripts builds kernel and uboot as well as fex file for Allwinner 
+# H3 as used for Orange Pi PC.
+# Steffen Graf S.Graf@gsg-elektronik.de
 
-cd brandy/u-boot-2011.09/
-make clean
-make -j4
-cd ../../tools/pack/chips/sun8iw7p1/bin
-cp ../../../../../brandy/u-boot-2011.09/u-boot.bin u-boot.fex
-../../../../../sunxi-tools/fex2bin ../configs/dolphin-p1/sys_config.fex ../configs/dolphin-p1/out/fex/sys_config.bin 
-../../../pctools/linux/mod_update/update_uboot u-boot.fex ../configs/dolphin-p1/out/fex/sys_config.bin
 
-#dd if=u-boot.fex of=/dev/sde bs=1k seek=16400
-#cp ../configs/dolphin-p1/out/fex/sys_config.bin $1
+function uboot-prepare {
+	cd brandy/u-boot-2011.09/
+	make mrproper
+	make sun8iw7p1_config
+	cd ../../
+}
 
-cd ../../../../../linux-3.4/
-#make orangepipc_defconfig
-make uImage -j4
-#cp arch/arm/boot/uImage $1
+function uboot-build {
+	cd brandy/u-boot-2011.09/
+	make -j4
+	cd ../../
+}
+
+function uboot-copy {
+	cd tools/pack/chips/sun8iw7p1/bin
+	cp ../../../../../brandy/u-boot-2011.09/u-boot.bin u-boot.fex
+	../../../../../sunxi-tools/fex2bin ../configs/dolphin-p1/sys_config.fex ../configs/dolphin-p1/out/fex/sys_config.bin 
+	../../../pctools/linux/mod_update/update_uboot u-boot.fex ../configs/dolphin-p1/out/fex/sys_config.bin
+	#dd if=u-boot.fex of=$1 bs=1k seek=16400
+	echo "U-Boot copied to $1"
+}
+
+function fex_gen {
+	cd tools/pack/chips/sun8iw7p1/bin
+	../../../../../sunxi-tools/fex2bin ../configs/dolphin-p1/sys_config.fex ../configs/dolphin-p1/out/fex/sys_config.bin
+	cp ../configs/dolphin-p1/out/fex/sys_config.bin $1/script.bin
+	echo "Copied to" $1/script.bin
+}
+
+function kernel_prepare {
+	cd linux-3.4/
+	make orangepipc_defconfig
+	cd ../
+}
+
+function kernel_menu {
+	cd linux-3.4/
+	make menuconfig
+	cd ../
+}
+
+function kernel_build {
+	cd linux-3.4/
+	make uImage -j4
+	cd ../
+}
+
+function kernel_copy {
+	cd linux-3.4/
+	cp arch/arm/boot/uImage $1
+	echo "Copied to" $1/uImage
+}
+
+case "$1" in
+	"uboot-prepare")
+		uboot-prepare
+		;;
+		
+	"uboot-build")
+		uboot-build
+		;;
+	
+	"uboot-copy")
+		if [ -z ${2+x} ]; 
+			then echo "specify sd card e.g. /dev/sde"; 
+			else uboot-copy $2; 
+		fi
+		;;
+		
+	"uboot-all")
+		if [ -z ${2+x} ]; 
+			then echo "specify sd card e.g. /dev/sde"; 
+			else 
+			uboot-prepare
+			uboot-build
+			uboot-copy $2; 
+		fi
+		;;
+		
+	"fex-copy")
+		if [ -z ${2+x} ]; 
+			then echo "specify path to mounted sdcard e.g. /media/sd"; 
+			else fex_gen $2; 
+		fi
+		;;
+		
+	"fex-edit")
+		editor tools/pack/chips/sun8iw7p1/configs/dolphin-p1/sys_config.fex
+		;;
+		
+	"kernel-prepare")
+		kernel_prepare
+		;;
+		
+	"kernel-menu")
+		kernel_menu
+		;;
+		
+	"kernel-build")
+		kernel_build
+		;;
+		
+	"kernel-copy")
+		if [ -z ${2+x} ]; 
+			then echo "specify path to mounted sdcard e.g. /media/sd"; 
+			else kernel_copy $2; 
+		fi
+		;;
+		
+	"kernel-all")
+		kernel_prepare
+		kernel_build
+		kernel_copy
+		;;
+	
+	*)
+		echo "uboot-prepare"
+		echo "uboot-build"
+		echo "uboot-copy"
+		echo "uboot-all"
+		echo "fex-copy"
+		echo "fex-edit"
+		echo "kernel-prepare"
+		echo "kernel-menu"
+		echo "kernel-build"
+		echo "kernel-copy"
+		exit
+	;;
+		
+esac
 
